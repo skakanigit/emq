6bc028905f658f014d8e00d6ac9f11288b7720da
Sriram Kakani <kakani.sriram@gmail.com>
2019-07-20 13:33:00 -0400

    Generated a maven project structure

---
+++ src/main/java/com/raptors/emq/EmqApplication.java
@@ -0,0 +1,13 @@
+package com.raptors.emq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmqApplication.class, args);
+	}
+
+}
